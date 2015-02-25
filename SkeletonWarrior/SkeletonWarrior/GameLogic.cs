909226eef7fdd1d49ebbe423c85231672173e630
vTodorov <Vasil.Todorov@outlook.com>
2015-02-25 23:42:28 +0200

    add menu when player gains a level

--- SkeletonWarrior/SkeletonWarrior/GameLogic.cs
+++ SkeletonWarrior/SkeletonWarrior/GameLogic.cs
@@ -44,6 +44,42 @@ namespace SkeletonWarrior
         public void LevelUpPlayer()
         {
             //Runs when the player kills enough enemies that he levels up. 
+
+            //menu
+            SetCursorPosition(0, 0);
+            Console.Write("You raised level!");
+            SetCursorPosition(18, 0);
+            Console.WriteLine("Choose from menu: ");
+            SetCursorPosition(36, 0);
+            Console.WriteLine("Press \"1\" for movement Speed,");
+            SetCursorPosition(66, 0);
+            Console.WriteLine("\"2\" for attack Power");
+            SetCursorPosition(88, 0);
+            Console.WriteLine("Press \"3\" for Firing Speed");
+            SetCursorPosition(36, 1);
+            Console.WriteLine("Press \"4\" for More Lives");
+
+            //player choice
+            bool commandCorrenct = false;
+            while (!commandCorrenct)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.D1: commandCorrenct = true; break;   // Do sonething 
+                        case ConsoleKey.D2: commandCorrenct = true; break;   // Do sonething 
+                        case ConsoleKey.D3: commandCorrenct = true; break;   // Do sonething 
+                        case ConsoleKey.D4: commandCorrenct = true; break;   // Do sonething 
+                        default: break;
+                    }
+                }
+            }
+        }
+        static void SetCursorPosition(int x, int y)
+        {
+            Console.SetCursorPosition(Console.BufferWidth - 120 + x, Console.BufferHeight - 40 + y);
         }
 
         public void SetBackgroundColorOnLevelChange()
