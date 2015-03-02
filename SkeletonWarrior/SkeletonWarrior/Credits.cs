3d53812ebc9feb3c6bc7b8a065033a1ee3892d50
muchcandidate <cropper88@abv.bg>
2015-03-03 00:04:39 +0200

    Add changes to Credit.cs

--- SkeletonWarrior/SkeletonWarrior/Credits.cs
+++ SkeletonWarrior/SkeletonWarrior/Credits.cs
@@ -8,5 +8,27 @@ namespace SkeletonWarrior
 {
     class Credits
     {
+        static void Main()
+        {
+            string endNames = "The participants in \"Skeleton Warrior\" game project:\n";
+
+            string[] arrayOfNames = new string[]{
+            "Nikolay Karagyozov",
+            "Ivailo Kolarov",
+            "Pavlina Dragneva",
+            "Ivan Donchev",
+            "Vasil Todorov",
+            "Krasimir Georgiev",
+            "Stefan  Dimitrov",
+            "Kaloian Koravski",
+        };
+
+            Console.WriteLine();
+            Console.WriteLine("{0}", endNames);
+            foreach (var day in arrayOfNames)
+            {
+                Console.WriteLine("{0}\n", day);
+            }
+        }
     }
 }
