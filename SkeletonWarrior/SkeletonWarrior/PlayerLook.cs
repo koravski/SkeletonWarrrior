3cecbcc58186eb00a70068e72c8945ca37b458fb
muchcandidate <cropper88@abv.bg>
2015-03-03 00:34:03 +0200

    The players look

---
+++ SkeletonWarrior/SkeletonWarrior/PlayerLook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkeletonWarrior
+{
+    class PlayerLook
+    {
+        static void Main(string[] args)
+        {
+            if (playerlevel <= 2)
+            {
+                playermodel = "-.☺.-";
+            }
+            else if (playerlevel <= 5)
+            {
+                playermodel = " -.☺.= ";
+            }
+            else if (playerlevel <= 9)
+            {
+                playermodel = " =.☺.= ";
+            }
+            else if (playerlevel <= 12)
+            {
+                playermodel = " -.☺.== ";
+            }
+            else
+            {
+                playermodel = " ==.☺.== ";
+            }
+        }
+    }
+}
